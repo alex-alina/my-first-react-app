70c7acc52bca23d0d4eddd5fb8a9ef0daa8f0ee8
Alina <alex.alina.rusu@gmail.com>
2019-01-14 15:57:50 +0100

    added lightbulbs images

--- src/components/LightSwitch.js
+++ src/components/LightSwitch.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import './LightSwitch.css';
 
 export default class LightSwitch extends Component {
   state = { active: true }
@@ -13,7 +14,8 @@ export default class LightSwitch extends Component {
     return (
       <div>
         <p>The light switch is <b>{ this.state.active ? 'on' : 'off' }</b></p>
-        <button onClick={this.toggle}>Toggle</button>
+        <img className="lightbulb" src={ this.state.active ? "https://openclipart.org/image/800px/svg_to_png/269989/Anthropomorphic-Cartoon-Light-Bulb.png" : 'https://openclipart.org/image/800px/svg_to_png/270125/Anthropomorphic-Cartoon-Dark-Bulb.png'} alt="Lightbulb" />
+        <button onClick={this.toggle}>Click the Switch</button>
       </div>
     )
   }
